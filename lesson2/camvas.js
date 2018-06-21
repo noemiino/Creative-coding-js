29b3cb18880be25ce7d89fcc2fdbdac47afcb40e
Noemi Biro <37864534+noemiino@users.noreply.github.com>
2018-06-21 15:42:15 +0200

    Delete camvas.js

--- lesson2/camvas.js
+++
@@ -1,7 +0,0 @@
-function createCanvas() {
-    var x = document.createElement("Canvas");
-    var ctx = x.getContext("2d");
-    ctx.fillStyle = "#FF0000";
-    ctx.fillRect(20, 20, 150, 100);
-    document.body.appendChild(x);
-}
